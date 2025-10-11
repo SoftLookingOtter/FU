4d6edb0c3cc11af983f9eb7e8239a02a3eb647b1
Sara Lindén <213674017+SoftLookingOtter@users.noreply.github.com>
2025-10-11 20:49:06 +0200

    Scanner added, final - class Game

--- TreIRad/src/se/saralinden/treirad/Game.java
+++ TreIRad/src/se/saralinden/treirad/Game.java
@@ -1,8 +1,16 @@
 package se.saralinden.treirad;
 
+import java.util.Scanner;
+
 // Game.java
 public class Game {
+
+    private final Scanner in = new Scanner(System.in); // private final = safe reuse
+    // private: only Game class can access this field
+    // final: the reference to "in" cannot be changed after assignment
+
     public void start() {
+        System.out.println();
         System.out.println("🦝 Raccoon Referee: Welcome to Tic-Tac-Toe!");
         System.out.println("Marks: ❌ vs ⭕. Choose cells 1–9. Ctrl+C to quit.");
 

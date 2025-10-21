85041e283da2a69e6f862d4af476d795ec70295e
Sara Lindén <213674017+SoftLookingOtter@users.noreply.github.com>
2025-10-21 14:39:43 +0200

    initial commit

--- ZooApp/src/zooapp/Swimmable.java
+++ ZooApp/src/zooapp/Swimmable.java
@@ -1,5 +1,5 @@
 package zooapp;
 
-interface Swimmable { // Interface för simmande djur
+interface Swimmable { // Interface för simmande dju
     void swim(); // Metod som måste implementeras av klasser som implementerar detta gränssnitt
 }
